933c46655e29e062081cc2989d59f177f2896336
cdrtim <phamngochai1257@gmail.com>
2017-01-11 11:10:01 +0700

    Initial Commit

---
+++ Sql_music/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sql_music
+//
+//  Created by Pham Ngoc Hai on 1/11/17.
+//  Copyright © 2017 pnh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
